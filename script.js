fa088b3405ad398da472b2761017db37dff26ec1
Francesco-De-Napoli27 <159804693+Francesco-De-Napoli27@users.noreply.github.com>
2025-07-09 19:53:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('navMenu');
+    const navMenu = document.getElementById('nav-menu');
 
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('active');
