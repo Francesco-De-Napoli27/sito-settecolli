1f3cd82d3bba956e68cbfa5942b94ed49c000d8f
Francesco-De-Napoli27 <159804693+Francesco-De-Napoli27@users.noreply.github.com>
2025-07-16 15:51:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -69,4 +69,21 @@ document.addEventListener('DOMContentLoaded', function () {
       closeMenu();
     }
   });
+
+  const reveals = document.querySelectorAll('.reveal');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+        observer.unobserve(entry.target); // attivazione una sola volta
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  reveals.forEach(element => {
+    observer.observe(element);
+  });
 });
